e7fa6e68b476ae824f31a1aa1a70b5d1899c7adf
dharien <dharien1317@gmail.com>
2025-06-29 17:55:11 -0500

    se agrego la funcionalidad buscar producto

--- registrar producto.cpp
+++ registrar producto.cpp
@@ -30,6 +30,22 @@ void listarProductos()
         cout<<i+1<<". "<<productos[i].nombre<<" - $"<<productos[i].precio<<endl;
     }
 }
+void buscarProducto()
+{
+    string nombre;
+    cout<<"Nombre del producto a buscar: ";
+    cin>>ws;
+    getline(cin, nombre);
+    for(int i=0;i<totalProductos;i++)
+    {
+        if(productos[i].nombre==nombre)
+        {
+            cout<<"Producto encontrado: "<<productos[i].nombre<<" - $"<<productos[i].precio<<endl;
+            return;
+        }
+    }
+    cout<<"Producto no encontrado."<<endl;
+}
 
 int main() 
 {
@@ -38,6 +54,7 @@ int main()
         cout<<"\nMenu:\n";
         cout<<"A. Registrar producto\n";
         cout<<"B. Listar productos\n";
+        cout<<"C. Buscar producto\n";
         cout<<"S. Salir\n";
         cout<<"Opcion: ";
         cin >> opcion;
@@ -46,6 +63,8 @@ int main()
             case 'A': registrarProducto(); 
             break;
             case 'B': listarProductos(); 
+            break;
+            case 'C': buscarProducto(); 
             break;
             case 'S': cout<<"Saliendo...\n"; 
             break;
